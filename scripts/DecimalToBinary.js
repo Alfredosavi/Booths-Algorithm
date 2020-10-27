056b604c40c4cb746ac8079805172a12df423b46
Alfredo Savi <alfredosavi@hotmail.com>
2020-10-27 15:03:24 -0300

    function Implementation (DecimalToBinary)

---
+++ scripts/DecimalToBinary.js
@@ -0,0 +1,24 @@
+function DecimalToBinary(decimal) {
+  var binary_resp = new String();
+  var binary_respTwoComp = new String();
+  var isNegative = new Boolean(false);
+
+  if (decimal < 0) {
+    decimal = Math.abs(decimal);
+    isNegative = true;
+  }
+
+  binary_resp = decimal.toString(2);
+  binary_resp = "0" + binary_resp;
+
+  if (isNegative === true) {
+    binary_respTwoComp = twoComplement(
+      binary_resp.slice(1, binary_resp.length)
+    );
+    binary_respTwoComp = "1" + binary_respTwoComp;
+
+    return binary_respTwoComp;
+  }
+
+  return binary_resp;
+}
