2abe6010b58a14537f346766fe95e7b0d4cd7b2b
Alfredo Savi <alfredosavi@hotmail.com>
2020-10-27 12:16:31 -0300

    function Implementation (BinaryToDecimal)

--- scripts/BinaryToDecimal.js
+++ scripts/BinaryToDecimal.js
@@ -1,1 +1,17 @@
-function BinaryToDecimal(bin) {}
+function BinaryToDecimal(bin) {
+  var decimal_resp = 0;
+  var isNegative = new Boolean(false);
+
+  if (bin[0] == "1") {
+    bin = twoComplement(bin);
+    isNegative = true;
+  }
+
+  decimal_resp = parseInt(bin, 2);
+
+  if (isNegative === true) {
+    decimal_resp = decimal_resp * -1;
+  }
+
+  return decimal_resp;
+}
