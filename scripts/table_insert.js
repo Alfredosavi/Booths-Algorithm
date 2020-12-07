37c0b32556b80b128c5d5b70a0c825ff5e64a666
Alfredo Savi <alfredosavi@hotmail.com>
2020-12-07 13:48:07 -0300

    Implementation of detachment and att lines

--- scripts/table_insert.js
+++ scripts/table_insert.js
@@ -18,7 +18,7 @@ function initializing() {
   newCell5.appendChild(document.createTextNode("Initial values"));
 }
 
-function rowUp(note) {
+function rowUp(operation, A, number_cycle) {
   var tbodyRef = document
     .getElementById("table")
     .getElementsByTagName("tbody")[0];
@@ -31,11 +31,29 @@ function rowUp(note) {
   var newCell4 = newRow.insertCell();
   var newCell5 = newRow.insertCell();
 
-  newCell1.appendChild(document.createTextNode());
-  newCell2.appendChild(document.createTextNode("00000000"));
-  newCell3.appendChild(document.createTextNode("0110"));
-  newCell4.appendChild(document.createTextNode("0"));
-  newCell5.appendChild(document.createTextNode("Initialized"));
+  newCell1.appendChild(document.createTextNode(operation));
+  newCell2.appendChild(document.createTextNode(A));
+  newCell3.appendChild(document.createTextNode());
+  newCell4.appendChild(document.createTextNode());
+  newCell5.appendChild(document.createTextNode(`${number_cycle}° Cycle`));
 }
 
-function rowDown() {}
+function rowDown() {
+  var tbodyRef = document
+    .getElementById("table")
+    .getElementsByTagName("tbody")[0];
+
+  var newRow = tbodyRef.insertRow();
+
+  var newCell1 = newRow.insertCell();
+  var newCell2 = newRow.insertCell();
+  var newCell3 = newRow.insertCell();
+  var newCell4 = newRow.insertCell();
+  var newCell5 = newRow.insertCell();
+
+  newCell1.appendChild(document.createTextNode("Arithmetic Shift -->"));
+  newCell2.appendChild(document.createTextNode(A));
+  newCell3.appendChild(document.createTextNode(Q));
+  newCell4.appendChild(document.createTextNode(QMO));
+  newCell5.appendChild(document.createTextNode(""));
+}
