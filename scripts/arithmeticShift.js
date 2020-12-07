1ee9b9e93ed8ed9618933ff2c355fd4784d91f36
Alfredo Savi <alfredosavi@hotmail.com>
2020-12-07 11:27:27 -0300

    ArithmeticShift

--- scripts/arithmeticShift.js
+++ scripts/arithmeticShift.js
@@ -1,23 +1,27 @@
-function arithmeticShift(A = "1001", Q = "0101", QMO = "0") {
+function arithmeticShift(A, Q, QMO) {
+  A = A.split("");
+  Q = Q.split("");
+
   let i = 0,
     A_L = A.length - 1,
     Q_L = Q.length - 1;
 
   QMO = Q[Q_L];
 
-  Q[3] = Q[2];
-  Q[2] = Q[1];
-  Q[1] = Q[0];
+  for (i = Q_L; i > 0; i--) {
+    Q[i] = Q[i - 1];
+  }
 
-  return Q;
+  Q[0] = A[A_L];
 
   for (i = A_L; i > 0; i--) {
     A[i] = A[i - 1];
   }
 
+  A = A.join("");
+  Q = Q.join("");
+
+  console.log(A);
   console.log(Q);
   console.log(QMO);
-  console.log(A);
 }
-
-// NOT WORK :(
